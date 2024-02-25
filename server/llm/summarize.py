27a8081e8c01919fbafc62a4e46d5fc6d34bd0ba
adikorlahalli <115048691+adikorlahalli@users.noreply.github.com>
2024-02-25 04:19:47 -0600

    fixed getting website text

--- server/llm/summarize.py
+++ server/llm/summarize.py
@@ -3,22 +3,32 @@ from bs4 import BeautifulSoup
 from transformers import pipeline
 
 def get_website_text(car_name: str):
-    google_search_query = '+'.join(car_name.split(' '))
-    google_request = requests.get(f'https://www.google.com/search?q=description+of+{google_search_query}')
-    google_soup = BeautifulSoup(google_request.text, features='html.parser')
-    
-    url = [
-        x['href']
-        for x in google_soup.find_all('a') 
-        if '/search' not in x['href'] 
-        and 'google' not in x['href'] 
-        and x['href'].startswith('/url?q=')
-    ][0]
+    splitter = car_name.split()
+    two_words = {'Aston Martin', 'Land Rover', 'MINI Cooper'}
+    body_type = {'Sedan', 'Hatchback', 'SUV', 'Coupe', 'Convertible', 'Wagon', 'Minivan', 'Van', 'Conv.'}
+    start = 1
+    if(' '.join(splitter[:2]) in two_words):
+      brand_name = f"{splitter[0].lower()}-{splitter[1].lower()}"
+      start+=1
+    else:
+      brand_name = splitter[0].lower()
 
-    car_request = requests.get(f'https://www.google.com/{url}')
+    model_name = ''
+    
+    '-'.join(splitter[start: -2]).lower()
+
+    for i in range(start, len(splitter) - 1):
+      if(splitter[i] in body_type):
+        break
+      model_name+=splitter[i].lower() + '-'
+    model_name = model_name[:-1]
+
+    year = splitter[-1]
+
+    car_request = requests.get(f'https://www.kbb.com/{brand_name}/{model_name}/{year}')
     car_soup = BeautifulSoup(car_request.text)
     car_text = car_soup.text
-
+    print(f'https://www.kbb.com/{brand_name}/{model_name}/{year}')
     return car_text
 
 def load_qa_pipeline():
